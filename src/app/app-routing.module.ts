a926b791c5177e00aa67c8b71f73c456aba764ec
khurram <youthpassion.com@gmail.com>
2022-10-04 23:59:06 +0500

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtComponent } from './art/art.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/art', pathMatch: 'full' },
+  { path: 'art', component: ArtComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/art/art.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-art',
+  templateUrl: './art.component.html',
+  styleUrls: ['./art.component.scss']
+})
+
+export class ArtComponent {
+  title = 'Shapespark';
+}
